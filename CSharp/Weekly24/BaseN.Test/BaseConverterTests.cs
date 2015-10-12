c99a8c25b8a39198c7102e5abe214357edf57727
Tonttu <tonttu86@msn.com>
2015-10-12 23:27:26 +0300

    Comment cleanup
    Cleaned up old commented out test cases.

--- CSharp/Weekly24/BaseN.Test/BaseConverterTests.cs
+++ CSharp/Weekly24/BaseN.Test/BaseConverterTests.cs
@@ -13,33 +13,20 @@ namespace BaseN.Test {
             new object[] {0, 10, new Number(new List<int> { 0 }, 10)},
             new object[] {1, 2, new Number(new List<int> { 1 }, 2)},
             new object[] {1, 10, new Number(new List<int> { 1 }, 10)},
-            //[TestCase(10, 2, Result = 1010)]
             new object[] {10, 2, new Number(new List<int> { 1, 0, 1, 0 }, 2)},
-            //[TestCase(10, 3, Result = 101)]
             new object[] {10, 3, new Number(new List<int> { 1, 0, 1 }, 3)},
-            //[TestCase(10, 4, Result = 22)]
             new object[] {10, 4, new Number(new List<int> { 2, 2 }, 4)},
-            //[TestCase(10, 5, Result = 20)]
             new object[] {10, 5, new Number(new List<int> { 2, 0 }, 5)},
-            //[TestCase(10, 6, Result = 14)]
             new object[] {10, 6, new Number(new List<int> { 1, 4 }, 6)},
-            //[TestCase(10, 7, Result = 13)]
             new object[] {10, 7, new Number(new List<int> { 1, 3 }, 7)},
-            //[TestCase(10, 8, Result = 12)]
             new object[] {10, 8, new Number(new List<int> { 1, 2 }, 8)},
-            //[TestCase(10, 9, Result = 11)]
             new object[] {10, 9, new Number(new List<int> { 1, 1 }, 9)},
             new object[] {10, 10, new Number(new List<int> { 1, 0 }, 10)},
-            //[TestCase(100, 2, Result=1100100)]
             new object[] {100, 2, new Number(new List<int> { 1, 1, 0, 0, 1, 0, 0 }, 2)},
-            //[TestCase(100, 5, Result = 400)]
             new object[] {100, 5, new Number(new List<int> { 4, 0, 0 }, 5)},
             new object[] {100, 10, new Number(new List<int> { 1, 0, 0 }, 10)},
-            //[TestCase(1000, 2, Result = 1111101000)]
             new object[] {1000, 2, new Number(new List<int> { 1, 1, 1, 1, 1, 0, 1, 0, 0, 0 }, 2)},
-            //[TestCase(1000, 5, Result = 13000)]
             new object[] {1000, 5, new Number(new List<int> { 1, 3, 0, 0, 0 }, 5)},
-            //[TestCase(1000, 10, Result = 1000)]
             new object[] {1000, 10, new Number(new List<int> { 1, 0, 0, 0 }, 10)},
             new object[] {9999, 10,  new Number(new List<int> { 9, 9, 9, 9 }, 10)},
             new object[] {999999999999, 10, new Number(new List<int> { 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 }, 10)},
